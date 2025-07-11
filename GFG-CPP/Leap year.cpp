8a21fb2da6652de1978da21da009330c03fd01fe
sandeep <sandeepmcodex.dev@gmail.com>
2025-07-11 23:49:46 +0530

    Add files via upload

---
+++ GFG-CPP/Leap year.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    bool checkYear(int n)
+    {
+        if (n % 400 == 0)
+            return 1;
+        else if (n % 4 == 0 && n % 100 != 0)
+            return 1;
+        else
+            return 0;
+    }
+};
